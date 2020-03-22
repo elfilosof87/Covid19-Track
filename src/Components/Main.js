38516b863b419580dcae3f0e745c952dea479433
Maaz Ahmed <elfilosof@protonmail.com>
2020-03-22 17:09:55 +0530

    initial setup

---
+++ src/Components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from "react";
+
+export default class main extends Component {
+  render() {
+    return (
+      <Fragment>
+        <center>
+          <h1 class="covid-text-map">Covid-19 Statistics and Information</h1>
+          <iframe
+            title="fram1"
+            src="https://public.domo.com/cards/bWxVg"
+            width="900"
+            height="600"
+            marginheight="0"
+            marginwidth="0"
+            frameborder="0"
+          ></iframe>
+        </center>
+      </Fragment>
+    );
+  }
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { Fragment, useState } from "react";
+import { BrowserRouter as Link } from "react-router-dom";
+import "../main";
+
+const Navbar = () => {
+  const [switchNav, toggleNav] = useState(false);
+  if (!switchNav) {
+    var nav1 = "covid-nav covid-nav-black visible";
+    var nav2 = "covid-nav covid-nav-red visible ";
+    var nav3 = "covid-nav covid-nav-white visible";
+    var nav4 = "covid-nav-btn covid-close-btn visible";
+  } else {
+    nav1 = "covid-nav covid-nav-black ";
+    nav2 = "covid-nav covid-nav-red ";
+    nav3 = "covid-nav covid-nav-white";
+    nav4 = "covid-nav-btn covid-close-btn ";
+  }
+
+  return (
+    <Fragment>
+      <button
+        className="covid-nav-btn covid-open-btn"
+        onClick={() => toggleNav(!switchNav)}
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <div className={nav1}>
+        <div className={nav2}>
+          <div className={nav3}>
+            <button className={nav4} onClick={() => toggleNav(!switchNav)}>
+              <i class="fas fa-times"></i>
+            </button>
+
+            <img
+              class="covid-logo"
+              src="https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_2/v1581911979/sas/wjpdipgnemywcwaq6hrx/COVID-19-webpage-logo.png"
+              alt="covidLogo"
+            />
+
+            <ul class="covid-list">
+              <li>
+                <Link href="#">Map</Link>
+              </li>
+              <li>
+                <Link href="#">History</Link>
+              </li>
+              <li>
+                <span class="act">
+                  <Link href="#">Prepare</Link>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+export default Navbar;
