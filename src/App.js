186d957a1e9fd6028397d41d6fc06af6d29d6ff4
Maaz Ahmed <elfilosof@protonmail.com>
2020-03-24 17:45:33 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
         <Route exact path="/prepare" component={Prepare} />
         <Route exact path="/prevent" component={Prevent} />
         <Route exact path="/faq" component={Faq} />
-        <Footer />
       </Fragment>
     </Router>
   );
