1b3ade36f2d4e12a3c82871e6699d9d8c9ab032a
Maaz Ahmed <44864105+elfilosof87@users.noreply.github.com>
2020-03-25 18:10:53 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -45,6 +45,9 @@ const Navbar = () => {
               <li>
                 <Link to="/india">India Live Map</Link>
               </li>
+              <li>
+                <Link to="/news">India News</Link>
+              </li>
 
               <li>
                 <span>
